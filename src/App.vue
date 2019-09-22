56320920cbd0f7fe6bc685b7d4ff5a9407fe48c8
Rishav Chourasia <rishav.chourasia@gmail.com>
2019-09-22 20:55:39 +0800

    feat: Added Image Uploader in website

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FaceAging/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FaceAging from './components/FaceAging.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FaceAging
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ImageUploader from "vue-image-upload-resize"
+
+Vue.use(ImageUploader)
 
 Vue.config.productionTip = false
 
